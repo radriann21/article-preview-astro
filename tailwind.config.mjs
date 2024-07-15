f2fffbed1c37b7d58da1e47a29a5f0d80c68ae0a
radriann21 <adrianrangelargu@gmail.com>
2024-07-15 12:50:45 -0400

    finished desktop and mobile styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'very-dark-grayish-blue': 'hsl(217, 19%, 35%)',
+				'desaturated-dark-blue': 'hsl(214, 17%, 51%)',
+				'grayish-blue': 'hsl(212, 23%, 69%)',
+				'light-grayish-blue': 'hsl(210, 46%, 95%)'
+			},
+			fontFamily: {
+				'global': "'Manrope', sans-serif"
+			}
+		},
 	},
 	plugins: [],
 }
